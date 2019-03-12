981db83c2e090c7033e4539f870eff76157dc89f
Stephen <27255268+StephenW75@users.noreply.github.com>
2019-03-12 16:37:01 -0400

    Update DirectoryServer.java

--- DirectoryServer.java
+++ DirectoryServer.java
@@ -10,13 +10,23 @@ public class DirectoryServer {
 		Hashtable<String, String> database = new Hashtable<String, String>();
 		ServerSocket socket = new ServerSocket(6789); 
 		while(true) {
+			
 			Socket connect = socket.accept(); 
+			
+			//Datastream from client via socket
 			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connect.getInputStream()));
 			
+			//Datastream to client via socket
 			DataOutputStream outToClient = new DataOutputStream(connect.getOutputStream());
+			
+			//read data retrieved from socket
 			itemName = inFromClient.readLine(); 
 			itemName = itemName.toLowerCase(); 
+			
+			//Get peer IP address from database
 			targetIp = database.get(itemName); 
+			
+			//Response to client
 			outToClient.writeBytes(targetIp); 
 			
 		}
